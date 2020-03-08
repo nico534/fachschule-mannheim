f4c5ce53b4a42e4ccb3aa4d37d7c49f6285a59a4
pc <nicolai-huening@gmx.de>
2020-03-08 11:47:39 +0100

    Auto slider update

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -9,8 +9,6 @@ slides = document.querySelectorAll(".slider figure img");
 
 slide_wrapper.style.width = (100 * (slides.length ) ) + "%";
 
-console.log(slide_wrapper.style.width);
-
 let width = (100 / slides.length);
 
 for(var i = 0; i < slides.length; i++){
@@ -20,12 +18,39 @@ for(var i = 0; i < slides.length; i++){
 let counter = 1;
 var maxCount = slides.length - 1;
 var canTranslate = true;
-let click_pos = -1;
+var posX1 = 0,
+    offset = 0,
+    posInitial,
+    posFinal,
+    threshold = 0.2,
+    second = 0;
 let interrupted = false;
 
 slide_wrapper.style.transform = 'translateX(' + (-width * counter) + '%)';
 
+/* Set Events */
+
+slide_wrapper.onmousedown = dragStart;
+
+slide_wrapper.addEventListener('transitionend', () => {
+  canTranslate = true;
+  slide_wrapper.style.transition = 'none';
+  if(counter == 0){
+    counter = maxCount - 1;
+    slide_wrapper.style.transform = 'translateX(' + (-width * counter) + '%)';
+  } else if (counter == (maxCount)) {
+    counter = 1;
+    slide_wrapper.style.transform = 'translateX(' + (-width * counter) + '%)';
+  }
+});
+
+slide_wrapper.addEventListener('touchstart', dragStart);
+slide_wrapper.addEventListener('touchend', dragEnd);
+slide_wrapper.addEventListener('touchmove', dragAction);
+
+/* Slide Functions */
 function left(){
+  second = 0;
   if(!canTranslate){
     return;
   }
@@ -37,56 +62,65 @@ function left(){
 }
 
 function right(){
+  second = 0;
   if(!canTranslate){
     return;
   }
   interrupted = true;
   canTranslate = false;
-  slide_wrapper.style.transition = "transform 0.4s ease-in-out";
+  slide_wrapper.style.transition = "transform 0.4s ease-out";
   counter++;
   slide_wrapper.style.transform = 'translateX(' + (-width * counter) + '%)';
 }
 
-slide_wrapper.addEventListener('touchstart', e => {
-  click_pos = e.offsetX;
-});
-slide_wrapper.addEventListener('touchend', e => {
-  if(click_pos < 0) return;
-  if(click_pos < e.offsetX){
-    left();
-  } else {
-    right();
-  }
-  click_pos = -1;
-});
 
-slide_wrapper.addEventListener('mousedown', e => {
-  click_pos = e.offsetX;
-});
-slide_wrapper.addEventListener('mouseup', e => {
-  if(click_pos < 0) return;
-    if(click_pos < e.offsetX){
-      left();
+function dragStart (e){
+  if(!canTranslate) return;
+  e = e || window.event;
+    e.preventDefault();
+    posInitial = counter;
+    if (e.type == 'touchstart') {
+      posX1 = e.touches[0].clientX;
     } else {
+      posX1 = e.clientX;
+      document.onmouseup = dragEnd;
+      document.onmousemove = dragAction;
+    }
+}
+
+function dragAction (e){
+  e = e || window.event;
+    if (e.type == 'touchmove') {
+      offset = (posX1 - e.touches[0].clientX);
+    } else {
+      offset = (posX1 - e.clientX);
+    }
+    counter = posInitial + (offset / slides[0].width);
+    slide_wrapper.style.transform = 'translateX(' + (-width * counter) + '%)';
+}
+
+function dragEnd (e){
+    if (counter - posInitial < -threshold) {
+      counter = posInitial;
+      left();
+    } else if (counter - posInitial > threshold) {
+      counter = posInitial;
+      right();
+    } else {
+      counter = posInitial - 1;
       right();
     }
-  click_pos = -1;
-});
+    document.onmouseup = null;
+    document.onmousemove = null;
+}
 
-slide_wrapper.addEventListener('transitionend', () => {
-  canTranslate = true;
-  if(counter == 0){
-    slide_wrapper.style.transition = 'none';
-    counter = maxCount - 1;
-    slide_wrapper.style.transform = 'translateX(' + (-width * counter) + '%)';
-  } else if (counter == (maxCount)) {
-    slide_wrapper.style.transition = 'none';
-    counter = 1;
-    slide_wrapper.style.transform = 'translateX(' + (-width * counter) + '%)';
+/* auto slider */
+
+window.setInterval(autoSlide, 1000);
+
+function autoSlide(){
+  second++;
+  if(second > 5){
+    right();
   }
-});
-
-function carousel() {
-  setTimeout(carousel, 8000); // Change image every 2 seconds
-  right();
 }
